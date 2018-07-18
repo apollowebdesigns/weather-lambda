1965cdc6492c9016e2df1dede235cba746ccb1b2
Andy Evans <andrewevans0@outlook.com>
2018-07-18 22:25:38 +0100

    updated http to dynamodb

--- handler.js
+++ handler.js
@@ -3,8 +3,22 @@ const moment = require('moment');
 const http = require('http');
 const {responseBuilder} = require('./modules/responsebuilder');
 const {httpcaller} = require('./modules/httpcaller');
+const AWS = require('aws-sdk');
+const documentClient = new AWS.DynamoDB.DocumentClient();
 
 module.exports.hello = (event, context, callback) => {
+    const params = {
+        TableName : 'ForecastTable',
+        Item: {
+            date: {
+                'done': 'done'
+            }
+        }
+    };
+    documentClient.put(params, (err, data) => {
+        if (err) console.log(err);
+        else console.log(data);
+    });
     let response = responseBuilder();
     httpcaller().then(res => {
         response.body = res;
